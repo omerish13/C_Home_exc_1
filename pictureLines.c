f0e3ec4b4f3ed2fffb664d39bb718e4f36439076
omerish13 <omerishai@gmail.com>
2024-02-01 23:48:45 +0200

    v10

--- pictureLines.c
+++ pictureLines.c
@@ -4,6 +4,113 @@
 #define ROWS 50
 #define COLS 50
 
+void addLines()
+{
+    int rows,cols;
+    int mat[ROWS][COLS] = {0};
+
+    printf("Mat ROWS: %d , COLS: %d\n", ROWS, COLS);
+
+    initMatSize(ROWS,COLS,&cols,&rows);
+
+    printf("Effective size %d*%d\n", rows, cols);
+
+    printf("Final picture after adding %d lines\n", countLines((int*)mat,cols,rows));
+    showPicture((int*)mat, cols, rows);
+    printf("\n");
+}
+
+void initMatSize(int maxRows, int maxCols,int* cols, int* rows)
+{
+    int res = 0;
+    while (res != 1)
+    {
+        printf("Insert number of rows for big Matrix between 1 and %d\n", maxRows);
+        scanf(" %d", rows);
+        if (*rows <= maxRows && *rows > 0)
+        {
+            res = 1;
+        }
+    }
+    res = 0;
+    while (res != 1)
+    {
+        printf("Insert number of columns for big Matrix: between 1 and %d\n", maxCols);
+        scanf(" %d", cols);
+        if (*cols <= maxCols && *cols > 0)
+        {
+            res = 1;
+        }
+    }
+ 
+}
+
+void showPicture(const int* mat, int cols, int rows)
+{
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			printf("%5d",*mat);
+			mat++;
+
+		}
+		printf("\n");
+	}
+}
+
+int countLines(int* mat, int cols, int rows)
+{
+    int count = 0, x1, x2, y1, y2, res;
+
+    int option = 1;
+
+	
+
+    do {
+        switch (option)
+        {
+        case 1:
+            res = getLine((int*)mat,cols,rows,&x1,&y1,&x2,&y2);
+            if (res)
+            {
+                if (x1 == x2 && y1 <= y2)
+                {  
+                    addVerticalLine((int*)mat,cols,rows,x1,y1,x2,y2);     
+                }
+
+                else if (x1 <= x2 && y1 == y2)
+                {
+                    addHorizontalLine((int*)mat,cols,rows,x1,y1,x2,y2);
+                }
+            }
+            count += res;
+
+        case 0:
+            break;
+        }
+	    printf("Do you want to enter a new line? 1 for yes, 0 no\n");
+
+	    scanf(" %d", &option);
+    } while (option != 0);
+
+    return count;
+}
+
+int getLine(int* mat, int cols, int rows, int* x1, int* y1, int* x2, int* y2)
+{
+    printf("Insert line parameters x1, y1, x2, y2\n");
+    scanf(" %d", x1);
+    scanf(" %d", y1);
+    scanf(" %d", x2);
+    scanf(" %d", y2);
+
+    if (checkLine((int*)mat,cols,rows,*x1,*y1,*x2,*y2))
+        return 1;     
+    return 0;
+
+}
+
 int checkLine(const int* mat, int cols, int rows,int x1,int y1, int x2, int y2)
 {
     if (y1 > (rows - 1) || y1 < 0 || y2 < 0 || y2 > (rows - 1) || x1 > (cols -1) || x1 < 0 || x2 > (cols - 1) || x2 < 0 || (x1 != x2 && y1 != y2) || x1 > x2 || y1 > y2)
@@ -57,112 +164,8 @@ void addVerticalLine(int* mat, int cols, int rows, int x1, int y1, int x2, int y
     }
 }
 
-int getLine(int* mat, int cols, int rows, int* x1, int* y1, int* x2, int* y2)
-{
-    printf("Insert line parameters x1, y1, x2, y2\n");
-    scanf(" %d", x1);
-    scanf(" %d", y1);
-    scanf(" %d", x2);
-    scanf(" %d", y2);
-
-    if (checkLine((int*)mat,cols,rows,*x1,*y1,*x2,*y2))
-        return 1;     
-    return 0;
-
-}
-
-int countLines(int* mat, int cols, int rows)
-{
-    int count = 0, x1, x2, y1, y2, res;
-
-    int option = 1;
-
-	
-
-    do {
-        switch (option)
-        {
-        case 1:
-            res = getLine((int*)mat,cols,rows,&x1,&y1,&x2,&y2);
-            if (res)
-            {
-                if (x1 == x2 && y1 <= y2)
-                {  
-                    addVerticalLine((int*)mat,cols,rows,x1,y1,x2,y2);     
-                }
-
-                else if (x1 <= x2 && y1 == y2)
-                {
-                    addHorizontalLine((int*)mat,cols,rows,x1,y1,x2,y2);
-                }
-                //showPicture((int*)mat, cols, rows);
-            }
-            count += res;
-
-        case 0:
-            break;
-        }
-	    printf("Do you want to enter a new line? 1 for yes, 0 no\n");
-
-	    scanf(" %d", &option);
-    } while (option != 0);
-
-    return count;
-}
 
 
-void initMatSize(int maxRows, int maxCols,int* cols, int* rows)
-{
-    int res = 0;
-    while (res != 1)
-    {
-        printf("Insert number of rows for big Matrix between 1 and %d\n", maxRows);
-        scanf(" %d", rows);
-        if (*rows <= maxRows && *rows > 0)
-        {
-            res = 1;
-        }
-    }
-    res = 0;
-    while (res != 1)
-    {
-        printf("Insert number of columns for big Matrix: between 1 and %d\n", maxCols);
-        scanf(" %d", cols);
-        if (*cols <= maxCols && *cols > 0)
-        {
-            res = 1;
-        }
-    }
- 
-}
 
-void showPicture(const int* mat, int cols, int rows)
-{
-	for (int i = 0; i < rows; i++)
-	{
-		for (int j = 0; j < cols; j++)
-		{
-			printf("%5d",*mat);
-			mat++;
 
-		}
-		printf("\n");
-	}
-}
-
-void addLines()
-{
-    int rows,cols;
-    int mat[ROWS][COLS] = {0};
-
-    printf("Mat ROWS: %d , COLS: %d\n", ROWS, COLS);
-
-    initMatSize(ROWS,COLS,&cols,&rows);
-
-    printf("Effective size %d*%d\n", rows, cols);
-
-    printf("Final picture after adding %d lines\n", countLines((int*)mat,cols,rows));
-    showPicture((int*)mat, cols, rows);
-    printf("\n");
-}
 
--- pictureLines.h
+++ pictureLines.h
@@ -3,12 +3,11 @@
 
 void addLines();
 void initMatSize(int maxRows, int maxCols,int* cols, int* rows);
-int checkHorizontalLine(const int* mat, int cols, int rows,int x1,int y1, int x2, int y2);
-int checkVerticalLine(const int* mat, int cols, int rows, int x1, int y1, int x2, int y2);
-int getLine(int* mat, int cols, int rows, int* x1, int* y1, int* x2, int* y2);
+void showPicture(const int* mat, int cols, int rows);
 int countLines(int* mat, int cols, int rows);
+int getLine(int* mat, int cols, int rows, int* x1, int* y1, int* x2, int* y2);
+int checkLine(const int* mat, int cols, int rows, int x1, int y1, int x2, int y2);
 void addHorizontalLine(int* mat, int cols, int rows, int x1, int y1, int x2, int y2);
 void addVerticalLine(int* mat, int cols, int rows, int x1, int y1, int x2, int y2);
-void showPicture(const int* mat, int cols, int rows);
 
 #endif
